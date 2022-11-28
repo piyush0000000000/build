62592f035c973f6ebb1c60f53d69bd20a75bcab5
piyush <tyagipiyush105@gmail.com>
2022-11-28 13:01:13 +0530

    Updates

---
+++ precache-manifest.ffedab41783e6669498cf99d48e01041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a975dd61529299a72764",
+    "url": "/build/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "a975dd61529299a72764",
+    "url": "/build/static/js/main.a975dd61.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/build/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/build/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "bed56e5adcfeac0b4ed7",
+    "url": "/build/static/js/runtime~main.bed56e5a.js"
+  },
+  {
+    "revision": "2e388fcf4ff7cdce22fae032e5883c4b",
+    "url": "/build/index.html"
+  }
+];
